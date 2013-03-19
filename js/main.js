25289824b109b02da179862ea0dd6865a3574fbb
mauricio <dottavio@gmail.com>
2013-03-19 17:57:02 -0300

    Fixing js to prevent login oauth2 issue

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Author: Mauricio Dottavio
-if(window.location.href.search(/google\.com\/reader\//gi) > -1){
+if((window.location.href.search(/google\.com\/reader\//gi) > -1) && (window.location.href.search(/oauth2/gi) == -1)){
 	window.location.href = 'http://www.feedly.com/home';
 }
